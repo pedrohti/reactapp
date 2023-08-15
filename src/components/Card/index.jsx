91d6692cf7b8f17ebc2203d6ddacb9bfe5d787d3
PedroH <pedroh.ti@hotmail.com>
2023-08-14 21:23:15 -0300

    Adicionando Estado

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,13 +1,10 @@
 import "./style.css";
 
-// export function Card(props) {
 export function Card({ name, time }) {
 	return (
 		<div className="card">
 			<strong>{name}</strong>
 			<small>{time}</small>
-			{/* <strong>{props.name}</strong>
-			<small>{props.time}</small> */}
 		</div>
 	);
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,25 @@
+import React, { useState } from "react";
 import "./styles.css";
 import { Card } from "../../components/Card";
 
 export function Home() {
+	const [studentName, setStudentName] = useState();
+
 	return (
 		<div className="container">
-			<h1>Lista de Presença</h1>
-			<input type="text" name="" id="" placeholder="Digite o nome..." />
+			<h1>Lista de Presença {studentName}</h1>
+
+			<input
+				type="text"
+				name=""
+				id=""
+				placeholder="Digite o nome..."
+				onChange={(e) => setStudentName(e.target.value)}
+			/>
 			<button type="button">Adicionar</button>
 			<Card name="Pedro" time="10:55:20" />
 			<Card name="Beatriz" time="13:15:10" />
+			<Card name="Ana" time="20:13:06" />
 		</div>
 	);
 }
